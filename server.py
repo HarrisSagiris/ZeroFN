ee53dd891fde640891c3eacc0880ab5b9cf90d46
DevHarris <161379408+HarrisSagiris@users.noreply.github.com>
2025-01-22 17:22:09 +0200

    Maybe working verison

--- server.py
+++ server.py
@@ -35,41 +35,46 @@ class FortniteAuthServer:
         self.match_instances = {}
 
     def load_cosmetics(self):
-        # Enhanced cosmetics list
+        # Enhanced cosmetics list with popular Fortnite items
         return {
             "characters": [
                 "CID_001_Athena_Commando_F_Default",
-                "CID_002_Athena_Commando_F_Default", 
-                "CID_003_Athena_Commando_F_Default",
-                "CID_004_Athena_Commando_F_Default",
-                "CID_005_Athena_Commando_M_Default",
-                "CID_006_Athena_Commando_M_Default",
-                "CID_007_Athena_Commando_M_Default",
-                "CID_008_Athena_Commando_M_Default",
-                # Add more character IDs here
+                "CID_286_Athena_Commando_F_NeonCat", # Lynx
+                "CID_313_Athena_Commando_M_KpopFashion", # IKONIK
+                "CID_434_Athena_Commando_F_StealthHonor", # Renegade Raider
+                "CID_028_Athena_Commando_F", # Brite Bomber
+                "CID_081_Athena_Commando_M_BlueDragon", # Blue Team Leader
+                "CID_175_Athena_Commando_M_Celestial", # Galaxy
+                "CID_342_Athena_Commando_M_StreetRacer", # Travis Scott
             ],
             "backpacks": [
-                "BID_001_BlackKnight",
-                "BID_002_RustLord", 
-                "BID_003_SpaceExplorer",
-                # Add more backpack IDs here
+                "BID_004_BlackKnight",
+                "BID_023_GhoulTrooper",
+                "BID_095_GalaxyStar",
+                "BID_122_RustLord",
+                "BID_142_SpaceExplorer",
             ],
             "pickaxes": [
-                "Pickaxe_Lockjaw",
-                "Pickaxe_Default",
-                # Add more pickaxe IDs here
+                "Pickaxe_ID_012_District", # Raider's Revenge
+                "Pickaxe_ID_013_Teslacoil", # AC/DC
+                "Pickaxe_ID_015_Holiday", # Candy Axe
+                "Pickaxe_ID_029_WinterCamo", # Cold Steel
             ],
             "gliders": [
-                "Glider_Default",
-                "Glider_Founder",
-                # Add more glider IDs here
+                "Glider_ID_001_Default",
+                "Glider_ID_002_Victory",
+                "Glider_ID_003_Founder",
+                "Glider_ID_008_StormSail",
             ],
             "emotes": [
                 "EID_DanceMoves",
-                "EID_Floss",
-                "EID_Fresh", 
+                "EID_Floss", 
+                "EID_TakeTheL",
+                "EID_Fresh",
                 "EID_RideThePony",
-                # Add more emote IDs here
+                "EID_Dab",
+                "EID_WormDance",
+                "EID_RobotDance"
             ]
         }
 
@@ -77,12 +82,13 @@ class FortniteAuthServer:
         try:
             self.server_socket.bind((self.host, self.port))
             self.server_socket.listen(5)
-            logger.info(f'Server listening on {self.host}:{self.port}')
+            logger.info(f'ZeroFN Server listening on {self.host}:{self.port}')
+            logger.info('Ready to accept Fortnite client connections')
 
             while self.running:
                 try:
                     client_socket, address = self.server_socket.accept()
-                    logger.info(f'New connection from {address}')
+                    logger.info(f'New Fortnite client connection from {address}')
                     client_thread = threading.Thread(target=self.handle_client, args=(client_socket, address))
                     client_thread.daemon = True
                     client_thread.start()
@@ -108,8 +114,19 @@ class FortniteAuthServer:
                 request_line = request_lines[0]
                 method, path, _ = request_line.split(' ')
 
-                # Always generate new auth token regardless of request
-                auth_token = self.generate_auth_token()
+                # Handle different API endpoints
+                response_body = ""
+                if "/account/api/oauth/token" in path:
+                    response_body = self.generate_auth_token_response()
+                elif "/fortnite/api/game/v2/profile" in path:
+                    response_body = self.generate_profile_response()
+                elif "/content/api/pages/fortnite-game" in path:
+                    response_body = self.generate_game_info()
+                elif "/lightswitch/api/service/bulk/status" in path:
+                    response_body = self.generate_lightswitch_response()
+                else:
+                    # Default auth response for unknown endpoints
+                    response_body = self.generate_auth_token_response()
 
                 # Prepare HTTP response headers
                 headers = [
@@ -120,9 +137,6 @@ class FortniteAuthServer:
                     'Access-Control-Allow-Headers: Content-Type, Authorization',
                     'Connection: keep-alive'
                 ]
-
-                # Always return successful auth response
-                response_body = self.generate_auth_token_response()
 
                 headers.append(f'Content-Length: {len(response_body)}')
                 response = '\r\n'.join(headers) + '\r\n\r\n' + response_body
@@ -151,19 +165,49 @@ class FortniteAuthServer:
             'refresh_token': token,
             'refresh_expires': 28800,
             'refresh_expires_at': '9999-12-31T23:59:59.999Z',
-            'account_id': 'ZeroFN',
+            'account_id': 'ZeroFN_Player',
             'client_id': 'ec684b8c687f479fadea3cb2ad83f5c6',
             'internal_client': True,
             'client_service': 'fortnite',
             'scope': 'basic_profile friends_list openid presence',
             'displayName': 'ZeroFN Player',
             'app': 'fortnite',
-            'in_app_id': 'ZeroFN',
-            'device_id': 'ZeroFN'
+            'in_app_id': 'ZeroFN_Player',
+            'device_id': 'ZeroFN_Device'
         })
 
+    def generate_profile_response(self):
+        return json.dumps({
+            "profileRevision": 1,
+            "profileId": "athena",
+            "profileChangesBaseRevision": 1,
+            "profileChanges": [],
+            "serverTime": datetime.now().isoformat(),
+            "responseVersion": 1
+        })
+
+    def generate_game_info(self):
+        return json.dumps({
+            "jcr:isCheckedOut": True,
+            "jcr:baseVersion": "a7ca237317f1e7883b3279-c38f-4aa7-a325-e099e4bf71e5",
+            "_title": "Fortnite Game",
+            "lastModified": datetime.now().isoformat(),
+            "_activeDate": datetime.now().isoformat(),
+            "_locale": "en-US"
+        })
+
+    def generate_lightswitch_response(self):
+        return json.dumps([{
+            "serviceInstanceId": "fortnite",
+            "status": "UP",
+            "message": "Fortnite is online",
+            "maintenanceUri": None,
+            "allowedActions": ["PLAY", "DOWNLOAD"],
+            "banned": False
+        }])
+
     def shutdown(self):
-        logger.info('Shutting down server...')
+        logger.info('Shutting down ZeroFN server...')
         self.running = False
         try:
             self.server_socket.close()
